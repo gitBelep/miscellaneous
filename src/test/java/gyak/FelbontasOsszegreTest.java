dd9335668ea2b36f242c993701ec7502f70f25ba
gitBelep <ddawid3@freemail.hu>
2021-06-29 16:34:23 +0200

    makeSumsTest

---
+++ src/test/java/gyak/FelbontasOsszegreTest.java
@@ -0,0 +1,28 @@
+package gyak;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FelbontasOsszegreTest {
+
+    @Test
+    void bontsdFel() {
+        FelbontasOsszegre f = new FelbontasOsszegre();
+        assertEquals(f.howMany(4,6), f.bontsdFel(4,6) );  //10 = 6+3+1
+
+//        f.bontsdFel(4,8);
+//        f.bontsdFel(4,7);  //20  = 10+6+3+1
+
+ //       f.howMany(4,6);
+ //       f.howMany(4,7);
+
+        assertEquals( f.howMany(2,6), f.bontsdFel(2,6) );  //5
+        assertEquals( f.howMany(2,7), f.bontsdFel(2,7) );  //6
+        assertEquals( f.howMany(2,8), f.bontsdFel(2,8) );  //7
+
+        assertEquals(f.howMany(3,6), f.bontsdFel(3,6) );  //10
+        assertEquals(f.howMany(3,7), f.bontsdFel(3,7));   //15
+        assertEquals(f.howMany(3,8), f.bontsdFel(3,8) );  //21
+    }
+}
