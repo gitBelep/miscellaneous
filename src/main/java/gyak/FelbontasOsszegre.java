1a07daf649e79cdd470c23ef0410fb9272e2e03e
gitBelep <ddawid3@freemail.hu>
2021-06-30 23:31:58 +0200

    TestTag

--- src/main/java/gyak/FelbontasOsszegre.java
+++ src/main/java/gyak/FelbontasOsszegre.java
@@ -2,7 +2,7 @@ package gyak;
 
 public class FelbontasOsszegre {
 
-    public int bontsdFel(int part, int number) {
+    public int bontsdFel1(int part, int number) {
         checkData(part, number);
         int a = 0;
         int b = 0;
@@ -94,81 +94,109 @@ public class FelbontasOsszegre {
         return sum;
     }
 
-//    public int bontsdFel(int part, int number) {
-//        checkData(part, number);
-//        int a = 0;
-//        int b = 0;
-//        int c;
-//        int d;
-//        int max = number - part + 1;
-//        int counter = 0;
-//
-//
-//        if (part == 2) {
-//            for (c = 1; c <= max - a - b - 2 + part; c++) {
-//                d = max - a - b - c - 1 + part;
-//                counter++;
-//                System.out.println(a + "+" + b + "+" + c + "+" + d);
-//            }
-//        }
-//
-//        if (part == 3) {
-//            for (b = 1; b <= max - a - 3 + part; b++) {
-//                for (c = 1; c <= max - a - b - 2 + part; c++) {
-//                    d = max - a - b - c - 1 + part;
-//                    counter++;
-//                    System.out.println(a + "+" + b + "+" + c + "+" + d);
-//                }
-//            }
-//        }
-//
-//        if (part == 4) {
-//            for (a = 1; a <= max; a++) {
-//                for (b = 1; b <= max - a - 3 + part; b++) {
-//                    for (c = 1; c <= max - a - b - 2 + part; c++) {
-//                        counter++;
-//                        d = max - a - b - c - 1 + part;
-//                        System.out.println(a + "+" + b + "+" + c + "+" + d);
-//                    }
-//                }
-//            }
-//        }
-//
-//        System.out.println(" *" );
-//        return counter;
-//    }
-//
-//    private void checkData(int part, int number) {
-//        if (number < part) {
-//            throw new IllegalArgumentException("A felbontandó szám legalább akkora legyen, mint a tagok száma!");
-//        }
-//        if (part < 2 || 4 < part) {
-//            throw new IllegalArgumentException("A tagok száma legalább 2 legfeljebb 4 legyen!");
-//        }
-//    }
-//
-//
-//    public int howMany(int part, int number) {
-//        int result = 0;
-//        if (part == 2) { return number - 1; }
-//        if (part == 3) { result = three(number); }
-//        if (part == 4) { result = four(number); }
-//        return result;
-//    }
-//
-//    private int three(int number) {
-//        int sum = 0;
-//        for (int i = number - 2; i > 0; i--) {
-//            sum += i;
-//        }
-//        return sum;
-//    }
-//
-//    private int four(int number) {
-//        int sum = 0;
-//        for (int k = number - 3; k > 0; k--) {
-//            sum += three(k + 2);
-//        }
-//        return sum;
-//    }
+    public int bontsdFel2(int part, int number) {
+        checkData(part, number);
+        int a = 0;
+        int b = 0;
+        int c;
+        int d;
+        int max = number - part + 1;
+        int counter = 0;
+
+
+        if (part == 2) {
+            for (c = 1; c <= max - a - b - 2 + part; c++) {
+                d = max - a - b - c - 1 + part;
+                counter++;
+                System.out.println(a + "+" + b + "+" + c + "+" + d);
+            }
+        }
+
+        if (part == 3) {
+            for (b = 1; b <= max - a - 3 + part; b++) {
+                for (c = 1; c <= max - a - b - 2 + part; c++) {
+                    d = max - a - b - c - 1 + part;
+                    counter++;
+                    System.out.println(a + "+" + b + "+" + c + "+" + d);
+                }
+            }
+        }
+
+        if (part == 4) {
+            for (a = 1; a <= max; a++) {
+                for (b = 1; b <= max - a - 3 + part; b++) {
+                    for (c = 1; c <= max - a - b - 2 + part; c++) {
+                        counter++;
+                        d = max - a - b - c - 1 + part;
+                        System.out.println(a + "+" + b + "+" + c + "+" + d);
+                    }
+                }
+            }
+        }
+
+        System.out.println(counter +"\n *");
+        return counter;
+    }
+
+    public int bontsdFel3(int part, int number){
+        checkData(part, number);
+        int result = -7;
+        if(part == 2) {
+            result = bont2(number);
+        }
+        if(part == 3) {
+            result = bont3(number);
+        }
+        if(part == 4) {
+            result = bont4(number);
+        }
+        System.out.println(result +"\n *");
+        return result;
+    }
+
+    private int bont2(int number){
+        int counter = 0;
+        for(int i = 1; i < number; i++){
+            for(int j = 1; j < number; j++){
+                if(i+j == number){
+                    System.out.println("0+0+"+ i +"+"+j);
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+
+    private int bont3(int number){
+        int counter = 0;
+        for(int i = 1; i < number; i++){
+            for(int j = 1; j < number; j++){
+                for(int k = 1; k < number; k++) {
+                    if (i + j + k == number) {
+                        System.out.println("0+" + i + "+" + j +"+"+ k);
+                        counter++;
+                    }
+                }
+            }
+        }
+        return counter;
+    }
+
+    private int bont4(int number){
+        int counter = 0;
+        for(int i = 1; i < number; i++){
+            for(int j = 1; j < number; j++){
+                for(int k = 1; k < number; k++) {
+                    for (int m = 1; m < number; m++) {
+                        if (i + j + k + m == number) {
+                            System.out.println(i + "+" + j + "+" + k + "+" + m);
+                            counter++;
+                        }
+                    }
+                }
+            }
+        }
+        return counter;
+    }
+
 }
--- src/test/java/gyak/FelbontasOsszegreTest.java
+++ src/test/java/gyak/FelbontasOsszegreTest.java
@@ -9,17 +9,17 @@ class FelbontasOsszegreTest {
     @Disabled("Same as the @RepeatedTest")
     void bontsdFel() {
         FelbontasOsszegre f = new FelbontasOsszegre();
-        assertEquals(f.howMany(4,6), f.bontsdFel(4,6) );  //10 = 6+3+1
-        assertEquals(f.howMany(4,7), f.bontsdFel(4,7) );  //20 = 10+6+3+1
-        assertEquals(f.howMany(4,8), f.bontsdFel(4,8) );  //35 = 15+10+6+3+1
+        assertEquals(f.howMany(4,6), f.bontsdFel1(4,6) );  //10 = 6+3+1
+        assertEquals(f.howMany(4,7), f.bontsdFel1(4,7) );  //20 = 10+6+3+1
+        assertEquals(f.howMany(4,8), f.bontsdFel1(4,8) );  //35 = 15+10+6+3+1
 
-        assertEquals( f.howMany(2,6), f.bontsdFel(2,6) );  //5
-        assertEquals( f.howMany(2,7), f.bontsdFel(2,7) );  //6
-        assertEquals( f.howMany(2,8), f.bontsdFel(2,8) );  //7
+        assertEquals( f.howMany(2,6), f.bontsdFel1(2,6) );  //5
+        assertEquals( f.howMany(2,7), f.bontsdFel1(2,7) );  //6
+        assertEquals( f.howMany(2,8), f.bontsdFel1(2,8) );  //7
 
-        assertEquals(f.howMany(3,6), f.bontsdFel(3,6) );  //10
-        assertEquals(f.howMany(3,7), f.bontsdFel(3,7));   //15
-        assertEquals(f.howMany(3,8), f.bontsdFel(3,8) );  //21
+        assertEquals(f.howMany(3,6), f.bontsdFel1(3,6) );  //10
+        assertEquals(f.howMany(3,7), f.bontsdFel1(3,7));   //15
+        assertEquals(f.howMany(3,8), f.bontsdFel1(3,8) );  //21
     }
 
     private int[][] testValues = {{4,6,10},{4,7,20},{4,8,35},{2,6,5},{2,7,6},{2,8,7},{3,6,10},{3,7,15},{3,8,21}};
@@ -28,17 +28,30 @@ class FelbontasOsszegreTest {
     void testBontsdFel(RepetitionInfo ri){
         FelbontasOsszegre f = new FelbontasOsszegre();
         assertEquals( testValues[ri.getCurrentRepetition()-1][2],
-                f.bontsdFel(testValues[ri.getCurrentRepetition()-1][0],testValues[ri.getCurrentRepetition()-1][1]) );
+                f.bontsdFel1(testValues[ri.getCurrentRepetition()-1][0],testValues[ri.getCurrentRepetition()-1][1]) );
     }
 
     @Test
-    @DisplayName("Bigger duty ~ 13 sec")
-    void naEztbontsdFel() {
+    @DisplayName("Bigger duty 1 ~ 6-13 sec")
+    void naEztbontsdFel1() {
         FelbontasOsszegre f = new FelbontasOsszegre();
-        assertEquals(1140, f.bontsdFel(4,21) );
-        assertEquals(1176, f.bontsdFel(3,50) );
-        assertEquals(1_499_784, f.bontsdFel(4,210) );
+        assertEquals(1_499_784, f.bontsdFel1(4,210) );
     }
+
+    @Test
+    @DisplayName("Bigger duty 3 ~ 67 sec")
+    void naEztbontsdFel3() {
+        FelbontasOsszegre f = new FelbontasOsszegre();
+        assertEquals(1_499_784, f.bontsdFel3(4,210) );
+    }
+
+    @Test
+    @DisplayName("Bigger duty 2 ~ 6-13 sec")
+    void naEztbontsdFel2() {
+        FelbontasOsszegre f = new FelbontasOsszegre();
+        assertEquals(1_499_784, f.bontsdFel2(4,210) );
+    }
+
 
     @Test
     @DisplayName("Testing throwing Exceptions")
@@ -46,12 +59,42 @@ class FelbontasOsszegreTest {
         FelbontasOsszegre f = new FelbontasOsszegre();
 
         IllegalArgumentException e1 = assertThrows(IllegalArgumentException.class,
-                () -> f.bontsdFel(1,7) );
+                () -> f.bontsdFel1(1,7) );
         IllegalArgumentException e2 = assertThrows(IllegalArgumentException.class,
-                () -> f.bontsdFel(4,3));
+                () -> f.bontsdFel1(4,3));
 
         assertEquals("A tagok száma legalább 2 legfeljebb 4 legyen!", e1.getMessage());
         assertEquals("A felbontandó szám legalább akkora legyen, mint a tagok száma!", e2.getMessage());
+    }
+
+    @Test
+    @Tag("Competition")
+    void testCompetition2(){
+        FelbontasOsszegre f = new FelbontasOsszegre();
+        assertAll(
+                () -> assertEquals(1140, f.bontsdFel2(4,21) ),
+                () -> assertEquals(19_600, f.bontsdFel1(4,51) ),
+                () -> assertEquals(1176, f.bontsdFel2(3,50) ));
+    }
+
+    @Test
+    @Tag("Competition")
+    void testCompetition1(){
+        FelbontasOsszegre f = new FelbontasOsszegre();
+        assertAll(
+                () -> assertEquals(1140, f.bontsdFel1(4,21) ),
+                () -> assertEquals(19_600, f.bontsdFel1(4,51) ),
+                () -> assertEquals(1176, f.bontsdFel1(3,50) ));
+    }
+
+    @Test
+    @Tag("Competition")
+    void testCompetition3(){
+        FelbontasOsszegre f = new FelbontasOsszegre();
+        assertAll(
+                () -> assertEquals(1140, f.bontsdFel3(4,21) ),
+                () -> assertEquals(19_600, f.bontsdFel1(4,51) ),
+                () -> assertEquals(1176, f.bontsdFel3(3,50) ));
     }
 
 }
