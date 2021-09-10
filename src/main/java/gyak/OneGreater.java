a83336c2e2516edea316a7a00111c163302b9d1b
gitBelep <ddawid3@freemail.hu>
2021-09-10 10:09:25 +0200

    numbers from digits

--- src/main/java/gyak/OneGreater.java
+++ src/main/java/gyak/OneGreater.java
@@ -1,14 +1,17 @@
 package gyak;
+/* Challenge: U'll get some digits in an int[] array. U could make many Integer numbers from them.
+Make a number from them as they are given in the array, and return the next greater variant.
+If U are given the greatest variant, return the smallest variant.   */
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class OneGreater {
 
-    public List<Integer> getOneGreater(int[] arr) {
+    public int getOneGreater(int[] arr) {
         List<Integer> ground = changeToList(arr);
-
-        for (int i = arr.length - 1; i >= 1; i--) {
+        int i;
+        for (i = arr.length - 1; i >= 1; i--) {
             if (arr[i - 1] < arr[i]) {
                 int smaller = arr[i - 1];
                 List<Integer> right = new ArrayList<>(arr.length - i + 1);
@@ -18,16 +21,24 @@ public class OneGreater {
                 right.sort(null);
                 int nextBiggerNumber = right.remove(right.lastIndexOf(smaller) + 1);
                 ground.set(i - 1, nextBiggerNumber);
+
                 int s = 0;
-                for ( ; i < arr.length; i++) {
+                for (; i < arr.length; i++) {
                     ground.set(i, right.get(s));
                     s++;
                 }
                 break;
             }
         }
-//        System.out.println("Result: " + ground);
-        return ground;
+
+        if (i == 0) {
+            ground.sort(null);
+//          System.out.println("Result: " + ground);
+            return makeNumberFromList(ground);
+        }
+
+//      System.out.println("Result: " + ground);
+        return makeNumberFromList(ground);
     }
 
     private List<Integer> changeToList(int[] a) {
@@ -38,4 +49,15 @@ public class OneGreater {
         return result;
     }
 
+    private int makeNumberFromList(List<Integer> list){
+        StringBuilder sb = new StringBuilder(list.size());
+        for(int i : list){
+            sb.append(i);
+        }
+        return Integer.parseInt(sb.toString());
+    }
+
 }
+/* MEGOLDÁS: Hátulról nézegetem a jegyeket. Ha a balra lévő nagyobb, megyek előrébb.
+Ha elfogynak, ez a legnagyobb kirakható szám. Ha találok kisebbet, akkor annak a helyére
+jön a nagyságban következő számjegy, mögötte minden növekvő sorrendbe kerül.   */
--- src/test/java/gyak/OneGreaterTest.java
+++ src/test/java/gyak/OneGreaterTest.java
@@ -1,5 +1,6 @@
 package gyak;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,7 +8,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class OneGreaterTest {
 
     @Test
-    void testOneGreater() {
+    @Disabled("Returning value changed to int")
+    void testOneGreaterToList() {
         OneGreater g = new OneGreater();
         assertEquals(Arrays.asList(1, 2, 3, 2), g.getOneGreater(new int[]{1, 2, 2, 3}));
         assertEquals(Arrays.asList(1, 3, 2, 2), g.getOneGreater(new int[]{1, 2, 3, 2}));
@@ -16,6 +18,25 @@ class OneGreaterTest {
         assertEquals(Arrays.asList(2, 2, 1, 3), g.getOneGreater(new int[]{2, 1, 3, 2}));
 
         assertEquals(Arrays.asList(4, 5, 1, 1, 2, 5), g.getOneGreater(new int[]{4, 2, 5, 5, 1, 1}));
+    }
+
+    @Test
+    void testOneGreaterToInt() {
+        OneGreater g = new OneGreater();
+        assertEquals(1232, g.getOneGreater(new int[]{1, 2, 2, 3}));
+        assertEquals(1322, g.getOneGreater(new int[]{1, 2, 3, 2}));
+        assertEquals(2123, g.getOneGreater(new int[]{1, 3, 2, 2}));
+        assertEquals(2132, g.getOneGreater(new int[]{2, 1, 2, 3}));
+        assertEquals(2213, g.getOneGreater(new int[]{2, 1, 3, 2}));
+
+        assertEquals(451125, g.getOneGreater(new int[]{4, 2, 5, 5, 1, 1}));
+    }
+
+    @Test
+    void testOneGreaterWithMax() {
+        OneGreater g = new OneGreater();
+        assertEquals(1223, g.getOneGreater(new int[]{3, 2, 2, 1}));
+        assertEquals(112455, g.getOneGreater(new int[]{5, 5, 4, 2, 1, 1}));
     }
 
 }
