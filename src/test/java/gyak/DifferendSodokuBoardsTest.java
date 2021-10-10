0c6cde7d3f7ab903f7e32b8ebdba575d3d4bdc1c
gitBelep <ddawid3@freemail.hu>
2021-10-10 22:45:45 +0200

    Same Musters in Corners -v1 -Test

---
+++ src/test/java/gyak/DifferendSodokuBoardsTest.java
@@ -0,0 +1,34 @@
+package gyak;
+
+import org.junit.jupiter.api.Test;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DifferendSodokuBoardsTest {
+
+    @Test
+    void testWithOneBoard(){
+        DifferendSodokuBoards sodoku = new DifferendSodokuBoards();
+        sodoku.makeStatisticsAboutCornerElements(Path.of(
+                "c:", "training", "miscellaneous", "src","main","resources","Sodoku-oneBoard.txt"));
+
+        assertEquals(List.of("63841752", "84137562", "75824361", "85137246"), sodoku.getStatistics());
+        assertEquals(Collections.emptyList(), sodoku.getSameMusters());
+    }
+
+    @Test
+    void testWithTwoBoardsTwoTimesSameMuster(){
+        DifferendSodokuBoards sodoku = new DifferendSodokuBoards();
+        sodoku.makeStatisticsAboutCornerElements(Path.of(
+                "c:", "training", "miscellaneous", "src","main","resources","Sodoku-twoBoards.txt"));
+
+        System.out.println(sodoku.getStatistics());
+        System.out.println(sodoku.getSameMusters());
+        assertEquals(List.of("63841752", "84137562", "75824361", "85137246",
+                "41235876", "7654231", "63841752", "75824361"), sodoku.getStatistics());
+        assertEquals(2, sodoku.getSameMusters().size());
+    }
+
+}
