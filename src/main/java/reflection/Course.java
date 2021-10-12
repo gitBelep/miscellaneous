4dbdceb8401955fd0f656efdd92180670dea0550
gitBelep <ddawid3@freemail.hu>
2021-10-12 12:05:22 +0200

    Reflection: modifying fields

--- src/main/java/reflection/Course.java
+++ src/main/java/reflection/Course.java
@@ -7,13 +7,17 @@ public class Course {
         this.name = name;
     }
 
-
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "course: '" + name + "\'";
     }
 
 }
--- src/main/java/reflection/ReflectionMain.java
+++ src/main/java/reflection/ReflectionMain.java
@@ -2,6 +2,7 @@ package reflection;
 
 import java.lang.reflect.*;
 import java.util.Arrays;
+import java.util.List;
 
 public class ReflectionMain<T> {
     private static Trainer trainer;
@@ -19,17 +20,55 @@ public class ReflectionMain<T> {
 //        main.oneMoreConstructor(trainer, main);
 //        main.methodParameters();
         main.aboutFields();
+        main.modifyingAPrivateAttribute(trainer, "java.util.List<reflection.Course>", "cours",
+                List.of(new Course("Java"), new Course("Python")));
+        main.modifyingAPrivateAttribute(trainer, "java.lang.String", "ame", "Newman Paula");
+        main.modifyingAPrivateAttribute(trainer, "int", "birt", 2000);
+        main.modifyingAPrivateAttribute(trainer, "int", "birt", "false data causes IllegalArgumentException");
     }
 
 
-    private void aboutFields(){
+    private void modifyingAPrivateAttribute(Trainer actualTrainer, String type, String part, Object newValue) {
         Field[] fields = Trainer.class.getDeclaredFields();
-        for(Field f : fields){
-            System.out.println(f.getName() +" ~ Modifier: "+ f.getModifiers() +"; "+f.getGenericType());
+        try {
+            for (Field actualField : fields) {
+                if (actualField.getGenericType().getTypeName().equals(type)
+                        && actualField.getName().contains(part)) {
+                    actualField.setAccessible(true);
+                    System.out.println("\n * Old value: " + actualField.get(actualTrainer)
+                            + " * Attribute changed: " + actualField.getName());
+                    actualField.set(actualTrainer, newValue);
+                }
             }
+        } catch (IllegalAccessException | IllegalArgumentException e) {
+            System.out.println("\"Now we are in the CATCH block\": "+ e);
+        } finally {
+            writeActualValuesOfAttributes(actualTrainer, fields);
+        }
     }
 
-    private void methodParameters(){
+    private void writeActualValuesOfAttributes(Trainer actualTrainer, Field[] fields) {
+        System.out.println("\nActual values:");
+        for(Field f : fields){
+            f.setAccessible(true);
+            try {
+                System.out.println(f.getName() +": "+ f.get(actualTrainer));
+            } catch (IllegalAccessException e) {
+                throw new IllegalStateException("Catch: ", e);
+            }
+        }
+    }
+
+    private void aboutFields() {
+        Field[] fields = Trainer.class.getDeclaredFields();
+        System.out.println("Listing all the fields:");
+        for (Field actualField : fields) {
+            System.out.println(actualField.getName() + " ~ Modifier: " + actualField.getModifiers()
+                    + "; " + actualField.getGenericType().getTypeName());
+        }
+    }
+
+    private void methodParameters() {
         try {
             Trainer emmi = Trainer.class.getConstructor().newInstance();
             Method ms = trainer.getClass().getMethod("setName", String.class);
@@ -37,42 +76,42 @@ public class ReflectionMain<T> {
 
             Method mg = Trainer.class.getMethod("getName");
             String names = ((String) mg.invoke(emmi)).concat(" & ").concat((String) mg.invoke(trainer));
-            System.out.println( names);
+            System.out.println(names);
 
-            System.out.println("parameter of setName: "+ ms.getParameters()[0].getName());
+            System.out.println("parameter of setName: " + ms.getParameters()[0].getName());
         } catch (Exception e) {
             throw new IllegalStateException("Cannot read method", e);
         }
     }
 
-    private void oneMoreConstructor(Trainer trainer, ReflectionMain main){
+    private void oneMoreConstructor(Trainer trainer, ReflectionMain main) {
         try {
             Constructor constr = trainer.getClass().getConstructor(String.class, int.class);
 
-            System.out.println("getDeclaringClass of the constructor(Str, int): "+ constr.getDeclaringClass());
+            System.out.println("getDeclaringClass of the constructor(Str, int): " + constr.getDeclaringClass());
             Class[] exes = constr.getExceptionTypes();
-            System.out.println("Length of the Array for getExceptionTypes of the constructor: "+ exes.length);
+            System.out.println("Length of the Array for getExceptionTypes of the constructor: " + exes.length);
         } catch (NoSuchMethodException e) {
             throw new IllegalArgumentException("Cannot read Constructor", e);
         }
     }
 
-    private void writeContsructors(){
+    private void writeContsructors() {
         Constructor[] constructors = Trainer.class.getConstructors();
 
-        for(int j = 0; j < constructors.length; j++){
+        for (int j = 0; j < constructors.length; j++) {
             Class[] paramTypes = constructors[j].getParameterTypes();
             Type[] types = constructors[j].getGenericParameterTypes();
-            System.out.println(j +": \n"+
-                    "Number of parameters: "+ constructors[j].getParameterCount() +"\n"+
-                    "Modifier: "+ constructors[j].getModifiers() +"\n"+
-                    Arrays.toString(paramTypes) +"\n"+
-                    Arrays.toString(types) +"\n");
+            System.out.println(j + ": \n" +
+                    "Number of parameters: " + constructors[j].getParameterCount() + "\n" +
+                    "Modifier: " + constructors[j].getModifiers() + "\n" +
+                    Arrays.toString(paramTypes) + "\n" +
+                    Arrays.toString(types) + "\n");
         }
     }
 
     private void aboutPackages() {
-        System.out.println( Human.class.getPackage().getName() +" = package of Human \n");
+        System.out.println(Human.class.getPackage().getName() + " = package of Human \n");
 
         Package[] packages = Package.getPackages();
         for (Package p : packages) {
@@ -80,51 +119,51 @@ public class ReflectionMain<T> {
         }
     }
 
-    private void writeMethodsOfTrainer(){
+    private void writeMethodsOfTrainer() {
         System.out.println("Public and inherited methods (from Human & Object) :");
         Method[] trPublMethods = Trainer.class.getMethods();
-        for(Method m : trPublMethods){
-            System.out.println(" "+ m);
+        for (Method m : trPublMethods) {
+            System.out.println(" " + m);
         }
 
         System.out.println("\nAll methods (declared in this class) :");
         Method[] trDeclMethods = Trainer.class.getDeclaredMethods();
-        for(Method m : trDeclMethods){
-            System.out.println(" declared: "+ m);
+        for (Method m : trDeclMethods) {
+            System.out.println(" declared: " + m);
         }
         System.out.println();
     }
 
-    private void writeClassDatas(Trainer.InnerSkills iSkill){
+    private void writeClassDatas(Trainer.InnerSkills iSkill) {
         System.out.println("Trainer.InnerSkills.getDeclaringClass() :");
-        System.out.println( iSkill.getClass().getDeclaringClass() );
+        System.out.println(iSkill.getClass().getDeclaringClass());
 
         Class iSkillClass = iSkill.getClass();
-        System.out.println(iSkillClass.isLocalClass() +" = Trainer.InnerSkill.class.isLocalClass()");
-        System.out.println(Trainer.InnerSkills.class.isMemberClass() +" = Trainer.InnerSkills.class.isMemberClass()");
+        System.out.println(iSkillClass.isLocalClass() + " = Trainer.InnerSkill.class.isLocalClass()");
+        System.out.println(Trainer.InnerSkills.class.isMemberClass() + " = Trainer.InnerSkills.class.isMemberClass()");
 
-        System.out.println( trainer.getClass().getName() +" = Trainer classname \n");
-        System.out.println( Course.class.getCanonicalName() +" = Course Canonical classname \n");
+        System.out.println(trainer.getClass().getName() + " = Trainer classname \n");
+        System.out.println(Course.class.getCanonicalName() + " = Course Canonical classname \n");
 
         System.out.println("Trainer.getDeclaredClasses() :");
         Class[] classesOfTrainer = Trainer.class.getDeclaredClasses();
-        for (Class c : classesOfTrainer){
-            System.out.println( c.getName() );
+        for (Class c : classesOfTrainer) {
+            System.out.println(c.getName());
         }
 
         System.out.println("\nSuperclass of Trainer :");
-        System.out.println( Trainer.class.getSuperclass().getTypeName());
+        System.out.println(Trainer.class.getSuperclass().getTypeName());
         System.out.println();
     }
 
     private void aboutModifiers(Trainer.InnerSkills iSkill) {
         System.out.println("\nModifier for Trainer & InnerSkills :");
         int modifier = Trainer.class.getModifiers();
-        System.out.println( modifier +": "+ Modifier.toString(modifier) );
-        System.out.println( Modifier.isPublic(modifier) +" = public \n");
+        System.out.println(modifier + ": " + Modifier.toString(modifier));
+        System.out.println(Modifier.isPublic(modifier) + " = public \n");
         int iSkillModifier = iSkill.getClass().getModifiers();
-        System.out.println( iSkillModifier +": "+ Modifier.toString(iSkillModifier) +" ~ myText: nothing ~");
-        System.out.println( Modifier.isProtected(iSkillModifier) +" = protected \n");
+        System.out.println(iSkillModifier + ": " + Modifier.toString(iSkillModifier) + " ~ myText: nothing ~");
+        System.out.println(Modifier.isProtected(iSkillModifier) + " = protected \n");
     }
 
 }
