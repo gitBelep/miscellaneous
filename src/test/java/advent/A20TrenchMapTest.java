59c979ff3e6a97a2ecb70aa0b862f98cf91e2811
gitBelep <ddawid3@freemail.hu>
2022-01-15 14:07:06 +0100

    AdventOfCode20

--- src/test/java/advent/A20TrenchMapTest.java
+++ src/test/java/advent/A20TrenchMapTest.java
@@ -7,11 +7,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class A20TrenchMapTest {
 
     @Test
-    void go() {
+    void goA() {
         Path path = Path.of("c:", "training", "miscellaneous", "src", "main", "resources", "advent", "20TrenchMap.txt");
         A20TrenchMap t = new A20TrenchMap();
 
         assertEquals(4873, t.go(path, 2));
+    }
+
+    @Test
+    void goB() {
+        Path path = Path.of("c:", "training", "miscellaneous", "src", "main", "resources", "advent", "20TrenchMap.txt");
+        A20TrenchMap t = new A20TrenchMap();
+
+        assertEquals(16394, t.go(path, 50));
     }
 
 }
