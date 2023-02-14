21105a01805c00c768bdc4ba668502850dfc4321
gitBelep <ddawid3@freemail.hu>
2023-02-14 19:03:59 +0100

    TreeSet

--- src/main/java/advent22/B01Calorie.java
+++ src/main/java/advent22/B01Calorie.java
@@ -3,54 +3,74 @@ package advent22;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.TreeSet;
 
 public class B01Calorie {
 
     public static void main(String[] args) {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(
                 B01Calorie.class.getResourceAsStream("b01input.txt")))) {
-            new B01Calorie().readLines(reader);
+
+            B01Calorie calorie = new B01Calorie();
+            calorie.findTop3Calories(reader);
+
         } catch (IOException ioe) {
             throw new IllegalStateException("Can not read file", ioe);
         }
     }
 
-    private void readLines(BufferedReader br) throws IOException {
+    private void findTop3Calories(BufferedReader br) throws IOException {
         String line = br.readLine();
-        int max = 0;
+        TreeSet<Integer> set = new TreeSet<>();
         int sum = 0;
+
         while (line != null) {
-            if (!line.trim().equals("")){
+            if (!line.trim().equals("")) {
                 sum += Integer.parseInt(line);
-            }
-            else {                            //border between elves
-                //System.out.print(sum + " ");
-                if (sum > max){
-                    max = sum;
-                    System.out.print(max + " "); //ascending maximums
-                }
+            } else {                            //border between elves
+                set.add(sum);
                 sum = 0;
             }
             line = br.readLine();
         }
 
-        System.out.println("\nMaximális kalóriaösszeg: " + max);
+        int actual;
+        String bests = "part 2: Maximal calories: ";
+        set.descendingSet();
+
+        actual = set.pollLast();
+        System.out.println("\npart 1: Maximális kalóriaösszeg: " + actual);
+        sum = actual;
+        bests = bests + Integer.toString(actual) + "; ";
+
+        actual = set.pollLast();
+        sum += actual;
+        bests = bests + Integer.toString(actual) + "; ";
+
+        actual = set.pollLast();
+        sum += actual;
+        bests = bests + Integer.toString(actual) + "; The total amount: " + Integer.toString(sum);
+
+        System.out.println(bests);
     }
 
 }
 
-//Santa's reindeer typically eat regular reindeer food, but they need a lot of magical energy to deliver presents on Christmas. For that, their favorite snack is a special type of star fruit that only grows deep in the jungle. The Elves have brought you on their annual expedition to the grove where the fruit grows.
-//
-//To supply enough magical energy, the expedition needs to retrieve a minimum of fifty stars by December 25th. Although the Elves assure you that the grove has plenty of fruit, you decide to grab any fruit you see along the way, just in case.
-//
-//Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
-//
-//The jungle must be too overgrown and difficult to navigate in vehicles or access from the air; the Elves' expedition traditionally goes on foot. As your boats approach land, the Elves begin taking inventory of their supplies. One important consideration is food - in particular, the number of Calories each Elf is carrying (your puzzle input).
-//
-//The Elves take turns writing down the number of Calories contained by the various meals, snacks, rations, etc. that they've brought with them, one item per line. Each Elf separates their own inventory from the previous Elf's inventory (if any) by a blank line.
-//
+//Santa's reindeer typically eat regular reindeer food, but they need a lot of magical energy to deliver presents
+// on Christmas. For that, their favorite snack is a special type of star fruit that only grows deep in the jungle.
+// The Elves have brought you on their annual expedition to the grove where the fruit grows.
+//To supply enough magical energy, the expedition needs to retrieve a minimum of fifty stars by December 25th.
+// Although the Elves assure you that the grove has plenty of fruit, you decide to grab any fruit you see along the way,
+// just in case.
+//Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar;
+// the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+//The jungle must be too overgrown and difficult to navigate in vehicles or access from the air; the Elves' expedition
+// traditionally goes on foot. As your boats approach land, the Elves begin taking inventory of their supplies.
+// One important consideration is food - in particular, the number of Calories each Elf is carrying (your puzzle input).
+//The Elves take turns writing down the number of Calories contained by the various meals, snacks, rations, etc.
+// that they've brought with them, one item per line. Each Elf separates their own inventory
+// from the previous Elf's inventory (if any) by a blank line.
 //For example, suppose the Elves finish writing their items' Calories and end up with the following list:
-//
 //1000
 //2000
 //3000
@@ -66,25 +86,24 @@ public class B01Calorie {
 //
 //10000
 //This list represents the Calories of the food carried by five Elves:
-//
 //The first Elf is carrying food with 1000, 2000, and 3000 Calories, a total of 6000 Calories.
 //The second Elf is carrying one food item with 4000 Calories.
 //The third Elf is carrying food with 5000 and 6000 Calories, a total of 11000 Calories.
 //The fourth Elf is carrying food with 7000, 8000, and 9000 Calories, a total of 24000 Calories.
 //The fifth Elf is carrying one food item with 10000 Calories.
-//In case the Elves get hungry and need extra snacks, they need to know which Elf to ask: they'd like to know how many Calories are being carried by the Elf carrying the most Calories. In the example above, this is 24000 (carried by the fourth Elf).
-//
+//In case the Elves get hungry and need extra snacks, they need to know which Elf to ask: they'd like to know
+// how many Calories are being carried by the Elf carrying the most Calories. In the example above,
+// this is 24000 (carried by the fourth Elf).
 //Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
-//
 //Your puzzle answer was 74394.
-//
-//The first half of this puzzle is complete! It provides one gold star: *
-//
 //--- Part Two ---
-//By the time you calculate the answer to the Elves' question, they've already realized that the Elf carrying the most Calories of food might eventually run out of snacks.
-//
-//To avoid this unacceptable situation, the Elves would instead like to know the total Calories carried by the top three Elves carrying the most Calories. That way, even if one of those Elves runs out of snacks, they still have two backups.
-//
-//In the example above, the top three Elves are the fourth Elf (with 24000 Calories), then the third Elf (with 11000 Calories), then the fifth Elf (with 10000 Calories). The sum of the Calories carried by these three elves is 45000.
-//
+//By the time you calculate the answer to the Elves' question, they've already realized that the Elf carrying the
+// most Calories of food might eventually run out of snacks.
+//To avoid this unacceptable situation, the Elves would instead like to know the total Calories carried
+// by the top three Elves carrying the most Calories. That way, even if one of those Elves runs out of snacks,
+// they still have two backups.
+//In the example above, the top three Elves are the fourth Elf (with 24000 Calories), then the third Elf
+// (with 11000 Calories), then the fifth Elf (with 10000 Calories).
+// The sum of the Calories carried by these three elves is 45000.
 //Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
+//Maximal calories: 74394; 69863; 68579; The total amount: 212836
